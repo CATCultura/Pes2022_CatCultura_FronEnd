991dbcd36204baf081ac89f959d31648eb1be51c
Superjuane <juan.emilio.olivan@estudiantat.upc.edu>
2022-11-16 12:17:18 +0100

    Interficie de Ruta Cultural (harcoded events but working II )

--- lib/repository/EventsRepository.dart
+++ lib/repository/EventsRepository.dart
@@ -1,3 +1,5 @@
+//import 'dart:html';
+
 import 'package:CatCultura/models/EventResult.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:CatCultura/data/network/networkApiServices.dart';
@@ -163,6 +165,20 @@ class EventsRepository {
     }
   }
 
+
+  Future<List<EventResult>> getRutaCultural(double longitud, double latitud, double radio) async {
+    // try{
+    //   dynamic response = await _apiServices.getGetApiResponse("${baseUrl}xxxxxxx");
+    //   List<EventResult> res = List.from(response.map((e) => EventResult.fromJson(e)).toList());
+    //   //_cachedEvents = res;
+    //   return res;
+    // } catch(e){
+    //   rethrow;
+    // }
+    //41.3874, 2.1686
+    await Future.delayed(const Duration(seconds: 2));
+    return [EventResult(id: "1", denominacio: "e1",longitud:41.3872, latitud: 2.1684),EventResult(id: "2", denominacio: "e2",longitud:41.3870, latitud: 2.1682),EventResult(id: "2", denominacio: "e2",longitud:41.3868, latitud: 2.1680)];
+  }
 
 
 
